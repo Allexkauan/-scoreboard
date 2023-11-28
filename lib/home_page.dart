e88b8cda8c52b0c5d0cbb0ef28acd0f612aa44c8
AlexKauan <alexpaivalima@hotmail.com>
2023-11-28 10:09:39 -0300

    add and remove buttons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 
+//  class Placar extends StatefulWidget {
+//    const Placar({super.key});
+
+//    @override
+//    State<Placar> createState() => _PlacarState();
+//  }
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text(
-          'Placar',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          title: const Text(
+            'Placar',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: const Center(
+        body: const Center(
           heightFactor: 300,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     'Team A',
@@ -41,7 +48,7 @@ class HomePage extends StatelessWidget {
                 ],
               ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     'Team B',
@@ -58,45 +65,65 @@ class HomePage extends StatelessWidget {
                 ],
               )
             ],
-          )),
-    );
+          ),
+        ),
+        floatingActionButton: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                onPressed: null,
+                tooltip: 'incrementA',
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: null,
+                tooltip: 'incrementA',
+                backgroundColor: Colors.red,
+                child: Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: null,
+                tooltip: 'incrementA',
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.restore),
+              ),
+              FloatingActionButton(
+                onPressed: null,
+                tooltip: 'incrementA',
+                backgroundColor: Colors.green,
+                child: Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: null,
+                tooltip: 'incrementA',
+                backgroundColor: Colors.red,
+                child: Icon(Icons.remove),
+              )
+            ]));
   }
 }
 
+//  class _PlacarState extends State<Placar> {
+//    int _teamA = 0;
+//    int _teamB = 0;
 
+//    void _incrementeTeamA() {
+//      setState(() {
+//        _teamA++;
+//      });
+//    }
 
+//    void _incrementeTeamB() {
+//      setState(() {
+//        _teamB++;
+//      });
+//    }
 
-
-
-
-
-//  class MyHomepage extends StatefulWidget {
-//   const MyHomepage({super.key});
-
-//   @override
-//   State<MyHomepage> createState() => _Placar();
-// }
-
-// class _Placar extends State<MyHomepage> {
-//   int _teamA = 0;
-//   int _teamB = 0;
-
-//   void _incrementeTeamA() {
-//     setState(() {
-//       _teamA++;
-//     });
+//    void _resetCounter() {
+//      setState(() {
+//        _teamA = 0;
+//        _teamB = 0;
+//      });
+//    }
 //   }
-
-//   void _incrementeTeamB() {
-//     setState(() {
-//       _teamB++;
-//     });
-//   }
-
-//   void _resetCounter() {
-//     setState(() {
-//       _teamA = 0;
-//       _teamB = 0;
-//     });
-//   }
-// }
