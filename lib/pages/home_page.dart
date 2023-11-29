6b2a6f894b1939e0ead1b62f648fc8442dcce49b
Alex <alexpaivalima@hotmail.com>
2023-11-29 10:23:29 -0300

    fix erro invalid const value of the methods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Placar extends StatefulWidget {
-  const Placar({super.key});
+  const Placar({Key? key}) : super(key: key);
 
   @override
   State<Placar> createState() => _PlacarState();
@@ -13,13 +13,13 @@ class _PlacarState extends State<Placar> {
 
   void _incrementeTeamA() {
     setState(() {
-      _teamA++;
+      _teamA = _teamA + 1;
     });
   }
 
   void _decrementA() {
     setState(() {
-      _teamA--;
+      _teamA = _teamA - 1;
     });
   }
 
@@ -32,13 +32,13 @@ class _PlacarState extends State<Placar> {
 
   void _incrementeTeamB() {
     setState(() {
-      _teamB++;
+      _teamB = _teamB + 1;
     });
   }
 
   void _decrementB() {
     setState(() {
-      _teamB--;
+      _teamB = _teamB - 1;
     });
   }
 
@@ -57,7 +57,7 @@ class _PlacarState extends State<Placar> {
             ),
           ),
         ),
-        body: const Center(
+        body: Center(
           heightFactor: 300,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -65,15 +65,15 @@ class _PlacarState extends State<Placar> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
+                  const Text(
                     'Team A',
                     style: TextStyle(
                       fontSize: 30,
                     ),
                   ),
                   Text(
-                    '0',
-                    style: TextStyle(
+                    '$_teamA',
+                    style: const TextStyle(
                       fontSize: 30,
                     ),
                   )
@@ -82,15 +82,15 @@ class _PlacarState extends State<Placar> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
+                  const Text(
                     'Team B',
                     style: TextStyle(
                       fontSize: 30,
                     ),
                   ),
                   Text(
-                    '0',
-                    style: TextStyle(
+                    '$_teamB',
+                    style: const TextStyle(
                       fontSize: 30,
                     ),
                   )
@@ -99,39 +99,38 @@ class _PlacarState extends State<Placar> {
             ],
           ),
         ),
-        floatingActionButton: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton(
-                onPressed: _incrementeTeamA,
-                tooltip: 'incrementA',
-                backgroundColor: Colors.blue,
-                child: Icon(Icons.add),
-              ),
-              FloatingActionButton(
-                onPressed: _decrementA,
-                tooltip: 'decrementA',
-                backgroundColor: Colors.red,
-                child: Icon(Icons.remove),
-              ),
-              FloatingActionButton(
-                onPressed: _resetCounter,
-                tooltip: 'reset',
-                backgroundColor: Colors.grey,
-                child: Icon(Icons.restore),
-              ),
-              FloatingActionButton(
-                onPressed: _incrementeTeamB,
-                tooltip: 'incrementA',
-                backgroundColor: Colors.green,
-                child: Icon(Icons.add),
-              ),
-              FloatingActionButton(
-                onPressed: _decrementB,
-                tooltip: 'incrementA',
-                backgroundColor: Colors.red,
-                child: Icon(Icons.remove),
-              )
-            ]));
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(
+            onPressed: _incrementeTeamA,
+            tooltip: 'incrementA',
+            backgroundColor: Colors.blue,
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _decrementA,
+            tooltip: 'decrementA',
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'reset',
+            backgroundColor: Colors.grey,
+            child: const Icon(Icons.restore),
+          ),
+          FloatingActionButton(
+            onPressed: _incrementeTeamB,
+            tooltip: 'incrementB',
+            backgroundColor: Colors.green,
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _decrementB,
+            tooltip: 'decrementB',
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.remove),
+          )
+        ]));
   }
 }
